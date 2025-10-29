2ead1bacf2904ad95095fa857687f21497bc1fe1
Fenix055 <DevilHanter05@gmail.com>
2025-10-30 01:33:28 +0300

    Create

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    /// Task 1
+    public static boolean leapYear (int year){
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+            return true;
+        }else {
+            return false;
         }
+    }
+
+    /// Task 2
+    public static void deviceCheck (int system, int year){
+        String systemName = "";
+        String version = "";
+
+        if (system == 0){
+            systemName = "iOS";
+        } else {
+            systemName = "Android";
+        }
+        if (year < 2015){
+            version = "облегченную";
+        } else {
+            version = "обычную";
+        }
+        System.out.println("Установите " + version + " версию приложения для " + systemName + " по ссылке");
+    }
+
+    /// Task 3
+    public static int deliveryDays (int distance){
+        return ((distance-20) / 40) + 1;
+
+    }
+
+    public static void main(String[] args) {
+        /// Task 1
+        int year = 1994;
+        if (leapYear(year)){
+            System.out.println(year + " год — високосный год");
+        }else {
+            System.out.println(year + " год — невисокосный год");
+        }
+
+        /// Task 2
+        deviceCheck(0, 2015);
+
+        /// Task 3
+        System.out.println(deliveryDays(60) + " дня доставки");
     }
 }
